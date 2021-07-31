ebaad6e10b70bdd1302acc9f78a8291f9658e0ff
yash singhaniya <yash.r.singhaniya.81@gmail.com>
2021-07-31 20:52:34 +0530

    Set environment variables

--- note_app/settings.py
+++ note_app/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hco3enuq$rtqf%zwl+!kry(n6!u%fcqlh-lod$@owrm=9o_v(v'
+#SECRET_KEY = 'django-insecure-hco3enuq$rtqf%zwl+!kry(n6!u%fcqlh-lod$@owrm=9o_v(v'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangonotetakingapp.herokuapp.com']
 
 
 # Application definition
