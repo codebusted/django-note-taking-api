006f8f1c4ad98599409a86f2a6842395f2302aed
yash singhaniya <yash.r.singhaniya.81@gmail.com>
2021-08-14 00:29:35 +0530

    Modified setting.py

--- note_app/settings.py
+++ note_app/settings.py
@@ -81,8 +81,15 @@ WSGI_APPLICATION = 'note_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'note_app',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
